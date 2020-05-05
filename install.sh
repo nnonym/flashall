1e5cf5d8411ffd4ad4e2347618e44ec0bc9d1010
nnonym <58051143+nnonym@users.noreply.github.com>
2020-05-05 12:09:17 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,3 +2,4 @@ sudo apt install software-properties-common dmg2img
 sudo add-apt-repository ppa:nilarimogard/webupd8
 sudo apt-get update
 sudo apt-get install woeusb
+sudo cp -r /$PWD/flash /usr/bin/
