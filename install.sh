7a65a7e9f83514c8ad67726b68bdef68f6539997
nnonym <58051143+nnonym@users.noreply.github.com>
2020-05-05 12:13:48 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,8 @@
+clear
 sudo apt install software-properties-common dmg2img
 sudo add-apt-repository ppa:nilarimogard/webupd8
 sudo apt-get update
 sudo apt-get install woeusb
 sudo cp -r /$PWD/flash /usr/bin/
+clear
+echo 'Installation complete! To start flashall type: flash'
