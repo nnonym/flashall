2ff14229ea781662de7d561bed0a59e77181fd3b
nnonym <58051143+nnonym@users.noreply.github.com>
2020-07-27 20:58:23 +0300

    Update install-debian.sh

--- install-debian.sh
+++ install-debian.sh
@@ -1,8 +1,8 @@
 clear
-sudo apt install software-properties-common dmg2img
+sudo apt install software-properties-common dmg2img -y
 sudo add-apt-repository ppa:nilarimogard/webupd8
 sudo apt-get update
-sudo apt-get install woeusb
+sudo apt-get install woeusb -y
 sudo cp -r /$PWD/flash /usr/bin/
 clear
 echo 'Installation complete! To start flashall type: flash'
