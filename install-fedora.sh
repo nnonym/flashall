9d2951a6d516034cc1e50f9f851b0e460e2655f5
nnonym <58051143+nnonym@users.noreply.github.com>
2020-08-06 18:13:20 +0300

    Update install-fedora.sh

--- install-fedora.sh
+++ install-fedora.sh
@@ -2,5 +2,6 @@ clear
 sudo dnf update
 sudo dnf install WoeUSB dmg2img
 sudo cp -r /$PWD/flash /usr/bin/
+sudo chmod +x /usr/bin/flash
 clear
 echo 'Installation complete! To start flashall type: flash'
