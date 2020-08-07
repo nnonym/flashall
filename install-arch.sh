c242be6206d926350f3d11a1f77a54145582c0d7
nnonym <58051143+nnonym@users.noreply.github.com>
2020-08-07 22:50:40 +0300

    Create install-arch.sh

---
+++ install-arch.sh
@@ -0,0 +1,6 @@
+sudo pacman -S pacaur dd dmg2img
+pacaur -S woeusb-git
+sudo cp -r /$PWD/flash /usr/bin/
+sudo chmod +x /usr/bin/flash
+clear
+echo 'Installation complete! To start flashall type: flash'
